0e65ee92c165b2699c43bddd67b19382130fd635
Nuwan <shanubaby82@gmail.com>
2024-12-15 19:11:28 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,16 +1,13 @@
 /**
-░█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀█ ─█▀▀█ ▀▀█▀▀ ░█─░█ ── ░█▀▄▀█ ░█▀▀▄ 
-░█▄▄█ ░█▄▄▀ ░█▄▄█ ░█▀▀▄ ░█▄▄█ ─░█── ░█▀▀█ ▀▀ ░█░█░█ ░█─░█ 
-░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name PPASIDU-MD
+* @authors Pasidu <https://github.com/prabathLK>
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
 * @link <https://github.com/prabathLK/PRABATHMD>
 * @version 2.1.2
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2023 pasidu. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
